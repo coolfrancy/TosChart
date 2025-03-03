d0556c25b3b6bfe5f700fe7fac8c4a13887d96e7
coolfrancy <154463480+coolfrancy@users.noreply.github.com>
2025-03-03 16:55:55 +0000

    put website file format

--- TosChart/cleaner/cleaner.py
+++ TosChart/cleaner/cleaner.py
@@ -3,6 +3,7 @@ def wash():
     import os
 
     uncleaned_data_path='/workspaces/TosChart/TosChart/uncleaned_data'
+    cleaned_data_path='/workspaces/TosChart/TosChart/cleaned_data'
     files=os.listdir(uncleaned_data_path)
 
     # Loop through each file
@@ -12,34 +13,40 @@ def wash():
             with open(uncleaned_data_path + '/' + strat, 'r', encoding='windows-1252', errors='replace') as rfile:
                 old = rfile.readlines()[1:]  # Skip the first line
 
+                #takes out the last three uneeded statements from the data
+                for i,line in enumerate(reversed(old)):
+                    if 'Max trade' in line:
+                        old=old[:-i]
+                        break
+                
+                
                 old.pop(1)  # Remove the second line
+                old.pop()
                 sid = old.pop(3)  # Pop the 4th line (index 3)
                 old[0] = old[0].replace('\n', ' ')  # Replace newline in the first line
                 old[0] += sid  # Append the SID to the first line
 
-            # Now write back to the file
-            with open(uncleaned_data_path + '/' + strat, 'w', encoding='windows-1252') as wfile:
-                for line in old:
-                    if line != '\n':  # Avoid writing empty newlines
-                        new_line = line.replace(';', ',')  # Replace semicolons with commas
-                        wfile.write(new_line)  # Write the modified line to the file
+            #makes sure the trailing line skips are taken out before calculating len
+            while old[-1]=='\n':
+                old.pop()
 
+            #make sure the file has enough data
+            if len(old)>=3:
+                # Now write back to the file
+                with open(cleaned_data_path + '/' + strat, 'w', encoding='windows-1252') as wfile:
+                    for line in old:
+                        if line != '\n':  # Avoid writing empty newlines and stocks without enough trades
+                            new_line = line.replace(';', ',')  # Replace semicolons with commas
+                            wfile.write(new_line)  # Write the modified line to the file
+            else:
+                os.remove(uncleaned_data_path + '/' + strat)
+
+        except IndexError:
+            os.remove(uncleaned_data_path + '/' + strat)
+            continue
         except Exception as e:
             print(f"Error processing {strat}: {e}")
             continue
 
-
-    try:
-        for strat in files:
-            with open(uncleaned_data_path+'/'+strat, 'r', encoding='windows-1252', errors='replace') as rfile:
-                old=rfile.readlines()
-                fix=old[:-3]
-
-            with open(uncleaned_data_path+'/'+strat,'w') as wfile:
-                wfile.write(''.join(fix))
-    except:
-        print(strat)
-        print(old)
-    
 if __name__=='__main__':
     wash()
